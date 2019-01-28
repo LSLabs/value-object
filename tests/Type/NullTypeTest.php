30c1b1c66ca3d133dd6442e7ed65a41c019eac40
Marcel Lohwasser-Spitzer <marcel.lohwasser-spitzer@posteo.de>
2019-01-28 19:17:52 +0100

    Fix test doubles naming in NullTypeTest
    Fix the naming of test doubles in NullTypeTest to fit with the formal
    definitions of dummy, stub, spy, mock and fake.

--- tests/Type/NullTypeTest.php
+++ tests/Type/NullTypeTest.php
@@ -30,17 +30,15 @@ class NullTypeTest extends TestCase
 
     public function test_isSame_ON_same_class_and_identical_toScalarOrNull_RETURNS_true(): void
     {
-        $mock = $this->createMock(NullType::class);
-        $mock->method('toScalarOrNull')->willReturn(null);
+        $compareObject = new NullType();
 
-        $this->assertTrue($this->stack->isSame($mock));
-        $this->assertTrue($this->stack->isSame($this->stack));
+        $this->assertTrue($this->stack->isSame($compareObject));
     }
 
     public function test_isSame_ON_not_same_class_RETURNS_false(): void
     {
-        $mock = $this->createMock(ToScalarOrNullInterface::class);
+        $dummy = $this->createMock(ToScalarOrNullInterface::class);
 
-        $this->assertFalse($this->stack->isSame($mock));
+        $this->assertFalse($this->stack->isSame($dummy));
     }
 }
